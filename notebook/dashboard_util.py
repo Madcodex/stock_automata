2e422c5976f8acc978278d5595defe0eae40f78d
akash verma <akashverma.bitcs2@gmail.com>
2024-04-12 00:06:31 +0530

    added other indices to trade

--- notebook/dashboard_util.py
+++ notebook/dashboard_util.py
@@ -5,9 +5,14 @@ import pandas as pd
 
 def buy_sell_order(kite,strike_price = None,ce_pe = None, price = None, trans_type = None,order_type = "MARKET",quantity = 50):
     print("Placed order: ", strike_price, ce_pe, price,trans_type, order_type,f"{strike_price}{ce_pe}")
+    if "SENSEX" in strike_price:
+        exchange = kite.EXCHANGE_BFO
+    else:
+        exchange = kite.EXCHANGE_NFO
+
     order = kite.place_order(
         variety = kite.VARIETY_REGULAR,
-        exchange = kite.EXCHANGE_NFO,
+        exchange = exchange,
         tradingsymbol = f"{strike_price}{ce_pe}",
         transaction_type = trans_type,
         quantity = quantity,
@@ -68,7 +73,7 @@ def reorder_column(df):
 
 
 def prev_day_oi(kite, oi_strike, itm_strike, atm_strike, otm_strike):
-    for i in range(1, 6):
+    for i in range(2, 6):
         day = datetime.datetime.now() - datetime.timedelta(days=i)
         start_dt_prev = day.strftime("%Y-%m-%d")
         end_dt_prev = day.strftime("%Y-%m-%d")
@@ -89,13 +94,22 @@ def prev_day_oi(kite, oi_strike, itm_strike, atm_strike, otm_strike):
     return df_prev
 
 
-
+# config variables 
+nifty_inst_token = 256265
+bn_inst_token = 260105
+fin_inst_token = 257801
+midcp_inst_token = 288009
+sensex_inst_token = 265
 
 def active_oi_chg(kite,expiry_march, start_dt, end_dt, time_frame="minute"):
     df_prev = pd.DataFrame()
-    ltp_dict = kite.ltp([256265, 260105])
-    nifty_price = ltp_dict["256265"]["last_price"]
-    banknifty_price = ltp_dict["260105"]["last_price"]
+    ltp_dict = kite.ltp([nifty_inst_token, bn_inst_token, fin_inst_token, midcp_inst_token, sensex_inst_token])
+    nifty_price = ltp_dict[str(nifty_inst_token)]["last_price"]
+    banknifty_price = ltp_dict[str(bn_inst_token)]["last_price"]
+    finnifty_price = ltp_dict[str(fin_inst_token)]["last_price"]
+    midcpnifty_price = ltp_dict[str(midcp_inst_token)]["last_price"]
+    sensex_price = ltp_dict[str(sensex_inst_token)]["last_price"]
+
     atm_strike = int(banknifty_price // 100 * 100)
     otm_strike = [atm_strike + i * 100 for i in range(1, 2)]
     itm_strike = [atm_strike - i * 100 for i in range(1, 2)]
@@ -150,3 +164,34 @@ def format_dataframe(df):
         # Assuming all columns you want to format are numeric
             formatted_df[col] = formatted_df[col].apply(indian_number_format)
     return formatted_df
+
+
+def get_strike_price(kite,default_strike,expiry,call_stike_dd,put_strike_dd, value_to_add):
+    strike_prices = [{'label': str(price), 'value': price} for price in range(default_strike-1000, default_strike+1000, value_to_add)]
+    if call_stike_dd not in [strike['value'] for strike in strike_prices]:
+        call_stike_dd = None
+        put_strike_dd = None
+
+    if call_stike_dd is None:
+        default_call_strike = default_strike
+    else:
+        default_call_strike = call_stike_dd
+    
+    if put_strike_dd is None:
+        default_put_strike = default_strike + value_to_add
+    else:
+        default_put_strike = put_strike_dd
+    if 'SENSEX' in expiry:
+        exchg = 'BFO'
+    else:
+        exchg = 'NFO'
+    symbol_ce = f"{exchg}:{expiry}{default_call_strike}CE"
+    symbol_pe = f"{exchg}:{expiry}{default_put_strike}PE"
+    ltp_dict = kite.ltp([symbol_ce, symbol_pe])
+    call_price = ltp_dict[symbol_ce]['last_price']
+    put_price = ltp_dict[symbol_pe]['last_price']
+
+    call_display = f"Call Price: {call_price}" if call_price is not None else "Select a Call Strike"
+    put_display = f"Put Price: {put_price}" if put_price is not None else "Select a Put Strike" 
+
+    return strike_prices, default_call_strike, default_put_strike, call_display, put_display
